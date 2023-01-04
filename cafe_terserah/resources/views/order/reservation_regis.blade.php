c992d356e8e3cc20efb706a987004e4e8adf3355
mitatha <90816272+mitatha@users.noreply.github.com>
2023-01-04 13:11:00 +0700

    tampilan

--- cafe_terserah/resources/views/order/reservation_regis.blade.php
+++ cafe_terserah/resources/views/order/reservation_regis.blade.php
@@ -8,13 +8,15 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    
 </head>
 
-<body class="bg-secondary">
+<body style="background : #EED6C4; background-size:cover">
+
     <div class="bg-light p-5 mx-auto rounded-4 position-absolute top-50 start-50 translate-middle" style="width: 400px;">
         <form action="/reservation/order/process" method="post">
             <div class="mb-3 d-flex justify-content-center align-items-center">
-                <h1>Order Dine-In</h1>
+                <h1>Reservation</h1>
             </div>
             <div class="mb-3">
                 <label for="customer_name" class="form-label">Name</label>
@@ -29,7 +31,7 @@
                 <input type="number" class="form-control" id="total_person" name="total_person" value="1" min="1" max="100">
             </div>
             <div class="d-flex justify-content-center align-items-center">
-                <button class="btn btn-primary" style="width: 150px;" type="submit">Button</button>
+                <button class="btn" style="width: 150px;color:#EED6C4;background:#483434" type="submit">Reservation</button>
             </div>
         </form>
     </div>
